0cf8de1670ab07482ffa6c0418b7e95101ae58f5
Adam Pruner <adampruner@Adams-MacBook-Pro.local>
2017-03-19 12:10:49 -0700

    added food item card list component

--- client/src/components/food-item-card-list.jsx
+++ client/src/components/food-item-card-list.jsx
@@ -1,1 +1,33 @@
 import React from 'react';
+import FoodItemsCard from './food-items-card';
+
+export default class FoodItemsCardList extends React.Component {
+
+  constructor(props) {
+    super(props);
+  };
+
+  render() {
+
+    var cardListStyle = {
+
+    };
+
+    return (
+      <div className="FoodItemsCardList">
+        <FoodItemsCard />
+        <FoodItemsCard />
+      </div>
+    );
+  }
+}
+
+FoodItemsCardList.displayName = 'FoodItemsCardList';
+
+var {func} = React.PropTypes;
+
+FoodItemsCardList.propTypes = {
+    loggedIn: func.isRequired
+}
+
+module.exports = FoodItemsCardList;
