fe2e771e2c41a6a59bb49a1e818189e2f92b54df
Adam Pruner <adampruner@Adams-MacBook-Pro.local>
2017-03-18 22:16:07 -0700

    Added background Image to home page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import FoodSearch from './FoodSearch';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import SignUpModal from './components/sign-up-modal';
+import Background from '../assets/healthy_food.jpg';
 
 class App extends Component {
   constructor() {
@@ -48,15 +49,27 @@ class App extends Component {
     );
   } */
 
+
+
   render() {
+    var sectionStyle = {
+      height: "700px",
+      backgroundSize: "cover",
+      flex: 1,
+      overflow: "hidden",
+      backgroundImage: "url(" + Background + ")"
+    };
+
     return (
-        <div className='App'>
+      <div className='App'>
+        <section style={ sectionStyle }>
           <div className='ui text container'>
             <MuiThemeProvider>
               <SignUpModal />
             </MuiThemeProvider>
           </div>
-        </div>
+        </section>
+      </div>
     );
   }
 }
